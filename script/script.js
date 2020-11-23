a5e445894586ac235e56d8ac818bd59a33cc88ed
Nikolaev Dmitry <nik.dima001@yandex.ru>
2020-11-23 22:10:22 +0300

    Вторая домашняя работа

--- script/script.js
+++ script/script.js
@@ -1,5 +1,28 @@
-var money, income, addExpensees, deposit, mission, period;
 
-alert('Hello world');
+let money = 60000;
+console.log(money);
 
-console.log('Hello');
+let income = 'Фриланс';
+console.log(income);
+
+let addExpenses = 'Интернет, Такси, Коммуналка';
+console.log(addExpenses.length);
+
+let deposit = true;
+console.log(deposit);
+
+let mission = 1000000;
+console.log('Цель заработать ' + mission + ' рублей');
+
+let period = 6;
+console.log('Период равен ' + period + ' месяцев');
+
+console.log(addExpenses.toLowerCase());
+
+console.log(addExpenses.split(', '));
+
+let budgetDay =  money / 30;
+console.log(budgetDay);
+
+
+
