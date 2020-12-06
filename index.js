dad06d6c2d761ceacaca4d8cf5d498934f97315b
Nikolaev Dmitry <nik.dima001@yandex.ru>
2020-12-06 22:59:00 +0300

    9 домашняя работа

---
+++ index.js
@@ -0,0 +1,20 @@
+const start = document.getElementById('start');
+console.log(start);
+
+const income_add = document.getElementsByTagName('button');
+console.log((income_add)[0]);
+
+const expenses_add = document.getElementsByTagName('button');
+console.log((expenses_add)[1]);
+
+const checkbox = document.querySelector('#deposit-check');
+console.log(checkbox);
+
+const text = document.querySelectorAll('.additional_expenses-item');
+console.log(text);
+
+const value = document.getElementsByClassName('result-total');
+console.log(value);
+
+const range = document.querySelectorAll('.period-select');
+console.log((range));
